1c308e654e26c4b5f0d168652fc14fa2b4190238
yahiro-1120 <ec2-user@ip-172-31-35-24.ap-northeast-1.compute.internal>
2024-09-09 06:42:49 +0000

    javascript練習

--- app/views/app.js
+++ app/views/app.js
@@ -1,1 +1,10 @@
-alert('Hello World');
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
